214a9cd987c06db5f0f5c7da01904ee43e5d96f5
Jake Meyers <jakermey@outlook.com>
2022-02-14 16:23:21 -0500

    Update wp-config-sample.php
    Modified for parameters to read from Azure App Service default Connection String

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -18,18 +18,35 @@
  * @package WordPress
  */
 
-// ** Database settings - You can get this info from your web host ** //
+//Using environment variables for DB connection information
+
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*DataSource=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*UserId=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+// ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define('DB_NAME', $connectstr_dbname);
 
-/** Database username */
-define( 'DB_USER', 'username_here' );
+/** MySQL database username */
+define('DB_USER', $connectstr_dbusername);
 
-/** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+/** MySQL database password */
+define('DB_PASSWORD', $connectstr_dbpassword);
 
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
+/** MySQL hostname */
+define('DB_HOST', $connectstr_dbhost);
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
